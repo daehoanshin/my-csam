8fe1f72341b8520a21498dbb0a2d8b8e7cf85fab
DaeHoan Shin <xbb123@naver.com>
2018-06-22 03:24:18 +0900

    initial commit

---
+++ src/main/java/net/csam/MyCsamApplication.java
@@ -0,0 +1,12 @@
+package net.csam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCsamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCsamApplication.class, args);
+	}
+}
